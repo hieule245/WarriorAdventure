8ca19e30eef6010f0ae8f59d675344aed97e4079
Lê Ngọc Hiếu <142372393+hieule245@users.noreply.github.com>
2024-12-16 10:30:01 +0700

    single jump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeReference] public float speed = 6f;
+    public float speed = 6f;
+    public float heightjump = 6f;
     private float movement;
+    public float slipspeed;
+    public bool isGroud = true;
     private SpriteRenderer sprite;
+    public Rigidbody2D rd;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +19,8 @@ public class Movement : MonoBehaviour
     {
         movement = Input.GetAxis("Horizontal");
         Flip();
+        Jump();
+        Slip();
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -22,7 +28,7 @@ public class Movement : MonoBehaviour
         transform.position += new Vector3(movement , 0f, 0f) * Time.fixedDeltaTime * speed;
     }
 
-    void Flip()
+    public void Flip()
     {
         if (movement < 0f)
         {
@@ -33,4 +39,31 @@ public class Movement : MonoBehaviour
             sprite.flipX = false;
         }
     }
+    
+    public void Jump()
+    {
+        if(Input.GetKey(KeyCode.Space) && isGroud == true)
+        {
+            rd.AddForce(new Vector2(0f, heightjump), ForceMode2D.Impulse);
+            isGroud = false;
+        }
+    }
+
+    public void Slip()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rd.AddForce(new Vector2(slipspeed, 0f), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGroud = true;
+        }
+    }
 }
+
